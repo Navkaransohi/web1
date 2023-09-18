cfa342bec666b009823be02e30746cb06f7190f5
Navkaransohi <sohinavkaran@gmail.com>
2023-09-18 14:02:25 -0400

    add views

---
+++ MvcMovie/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name = "John", int ID = 1)
+        {
+            return "Your name is: " + name + " and Your ID is :" + ID;
+        }
+    }
+}
